8c1ef5fc5b22453a3b0353ff15cd72f236b8fffb
Rutger Nijhuis <rutger@nyhuis.dev>
2025-07-16 13:54:15 +0200

    feat: add test plugin

---
+++ FlowPluginsTs/CommunityFlowPlugins/dank/dankPlexTest/1.0.0/index.ts
@@ -0,0 +1,38 @@
+import {
+  IpluginDetails,
+  IpluginInputArgs,
+  IpluginOutputArgs,
+} from '../../../../FlowHelpers/1.0.0/interfaces/interfaces';
+
+const details = (): IpluginDetails => ({
+  name: 'Dank Plex Test',
+  description: 'Dank Plex Test Plugin',
+  style: {
+    borderColor: 'orange',
+  },
+  tags: '',
+  isStartPlugin: false,
+  pType: '',
+  sidebarPosition: 0,
+  icon: '',
+  inputs: [],
+  outputs: [
+    {
+      number: 1,
+      tooltip: 'Test output',
+    },
+  ],
+  requiresVersion: '2.11.01',
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const plugin = (args: IpluginInputArgs): IpluginOutputArgs => {
+  args.jobLog('Dank Plex Test Plugin executed');
+  return ({
+    outputFileObj: args.inputFileObj,
+    outputNumber: 1,
+    variables: args.variables,
+  });
+};
+
+export { details, plugin };
