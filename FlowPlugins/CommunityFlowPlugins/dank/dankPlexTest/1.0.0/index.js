0e916e9d395a706b7d880c8e7a793d937ba94ca1
Rutger Nijhuis <rutger@nyhuis.dev>
2025-07-16 13:57:47 +0200

    feat: compiled test plugin

---
+++ FlowPlugins/CommunityFlowPlugins/dank/dankPlexTest/1.0.0/index.js
@@ -0,0 +1,34 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.plugin = exports.details = void 0;
+var details = function () { return ({
+    name: 'Dank Plex Test',
+    description: 'Dank Plex Test Plugin',
+    style: {
+        borderColor: 'orange',
+    },
+    tags: '',
+    isStartPlugin: false,
+    pType: '',
+    sidebarPosition: 0,
+    icon: '',
+    inputs: [],
+    outputs: [
+        {
+            number: 1,
+            tooltip: 'Test output',
+        },
+    ],
+    requiresVersion: '2.11.01',
+}); };
+exports.details = details;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var plugin = function (args) {
+    args.jobLog('Dank Plex Test Plugin executed');
+    return ({
+        outputFileObj: args.inputFileObj,
+        outputNumber: 1,
+        variables: args.variables,
+    });
+};
+exports.plugin = plugin;
